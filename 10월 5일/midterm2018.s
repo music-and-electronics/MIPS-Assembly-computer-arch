9aed44ba4073a9df59dce176d4ebf32d347e8cee
김종현 <kjh970407@naver.com>
2019-10-05 17:56:27 +0900

    fix error

--- 10월 5일/midterm2018.s
+++ 10월 5일/midterm2018.s
@@ -9,13 +9,19 @@ ptr4: .asciiz "\n"
 main:
     la    $a0,ptr1
     la    $t0,ptr2
+    lw    $t0,0($t0)
     sll   $t0,$t0,2
     addi  $t0,$t0,-4
     add   $a1,$a0,$t0
     jal   label1
     la    $a0,ptr4
+    li    $v0,4
+    syscall
+    la    $a0,ptr1
     jal   label4
-
+    jal   label1
+    li    $v0,10
+    syscall
 
 label1:
     addi  $t0,$a0,0
@@ -46,7 +52,7 @@ label5:
     jal   label7
     lw    $t0,0($a0)
     sw    $t0,0($v0)
-    sw    $v1,0($v1)
+    sw    $v1,0($a0)
     addi  $a0,$a0,4
     j     label5
 
@@ -58,7 +64,7 @@ label6:
 label7:
     addi  $t0,$a0,0
     addi  $v0,$a0,0
-    lw    $v1,0($a1)
+    lw    $v1,0($a0)
 
 label8:
     beq   $t0,$a1,label9
